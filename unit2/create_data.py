5c2d75d5481ae95e28fcbf08d8a6cc711c418eec
Sameer Kulkarni <ksameersrk@gmail.com>
2016-09-28 18:20:45 +0530

    removed newline

--- unit2/create_data.py
+++ unit2/create_data.py
@@ -1,8 +1,13 @@
 import random
 from random import randint
-
+tmp = []
 for i in range(10000):
     size = randint(2,10)
     ls = random.sample(xrange(0,31), size)
     ls = list(map(str,ls))
-    print " ".join(ls)
+    tmp.append(" ".join(ls))
+    
+for i in range(len(tmp)-1):
+    print(tmp[i])
+    
+print(tmp[-1])
