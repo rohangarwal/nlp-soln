9b82a7f08eeb33f035fb74551bf0344b94c55779
Sameer Kulkarni <ksameersrk@gmail.com>
2016-09-28 18:28:07 +0530

    command line value N

--- unit2/create_data.py
+++ unit2/create_data.py
@@ -1,13 +1,16 @@
 import random
 from random import randint
+import sys
+
+N = int(sys.argv[1])
 tmp = []
-for i in range(10000):
+for i in range(N):
     size = randint(2,10)
     ls = random.sample(xrange(0,31), size)
     ls = list(map(str,ls))
     tmp.append(" ".join(ls))
     
-for i in range(len(tmp)-1):
-    print(tmp[i])
-    
-print(tmp[-1])
+ftrain = open("train_"+str(N)+".in", "w")
+ftest = open("test_"+str(N)+".in", "w")   
+ftrain.write("\n".join(tmp[:int(N*0.8)]))
+ftest.write("\n".join(tmp[int(N*0.8):]))
