9af2a61bd5f9c83c36df2d1c011cb4328638554e
Rohan <rohanagarwal6@gmail.com>
2016-11-09 18:08:03 +0530

    changes

--- unit3/NLP/test_file.py
+++ unit3/NLP/test_file.py
@@ -1,32 +1,38 @@
-def fwd(review):
-  for t in xrange(len(review)):
-  	xs[t] = review[t]
-    hs[t] = np.tanh(np.dot(Wxh, xs[t]) + np.dot(Whh, hs[t-1]) + bh) # hidden state
-    '''
-    xsf[t] = np.zeros((vocab_size,1)) # encode in 1-of-k representation
-    xsf[t][inputs[t]] = 1
-    hsf[t] = np.tanh(np.dot(Wxhf, xsf[t]) + np.dot(Whhf, hsf[t-1]) + bhf) # hidden state
-  	'''
-  return hs
+import pickle
+import numpy as np
 
-def test(review):
-	hf = fwd(review,hprev)
-	temp = []
-  	for t in range(len(review)):
-    	ys[t] = np.dot(Whyf, hf[t]) + np.dot(Whyb, hb[t]) + by # unnormalized log probabilities for next chars
-    	ps[t] = np.exp(ys[t]) / np.sum(np.exp(ys[t])) # probabilities for next chars
-    	temp.append(str(np.argmax(ps[t])))
+def fwd(review,hprev):
+    vector_length = 32
+    xs = {}
+    hs = {}
+    hs[-1] = hprev
+    for t in xrange(len(review)):
+        xs[t] = np.zeros((vector_length,1))
+        for j in range(32):
+            xs[t][j] = review[t][j]
+        hs[t] = np.tanh(np.dot(Wxh, xs[t]) + np.dot(Whh, hs[t-1]) + bh) # hidden state
+    return hs[(len(review)-1)]
 
-  return ' '.join(temp)
+def test(review,hprev):
+    hs = fwd(review,hprev)
+    ys = np.dot(Why, hs) + by
+    ps = np.exp(ys) / np.sum(np.exp(ys))
+    print ps
+    return str(np.argmax(ps))
 
 if __name__ == "__main__":
+    posreviews = pickle.load(open('../word2vec/pos_vec_test.pkl',"rb"))
+    negreviews = pickle.load(open('../word2vec/neg_vec_test.pkl',"rb"))
 
-	fp = open('trained_model.pkl','rb')
-		parameter_dict = pickle.load(fp)
-		hprev = parameter_dict['hprev']
-		Why = parameter_dict['Why']
-		by = parameter_dict['by']
-		Wxh = parameter_dict['Wxh']
-		Whh = parameter_dict['Whh']
-		bh = parameter_dict['bh']
-	fp.close()
+    parameter_dict = {}
+    fp = open('trained_model.pkl','rb')
+    parameter_dict = pickle.load(fp)
+    hprev = parameter_dict['hprev']
+    Why = parameter_dict['Why']
+    by = parameter_dict['by']
+    Wxh = parameter_dict['Wxh']
+    Whh = parameter_dict['Whh']
+    bh = parameter_dict['bh']
+    fp.close()
+    for review in negreviews:
+        print test(review,hprev)
