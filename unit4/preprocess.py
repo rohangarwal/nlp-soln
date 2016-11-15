5a589a6316e9124e66655a9a02782091a1f6215d
Sameer Kulkarni <ksameersrk@gmail.com>
2016-11-15 15:49:57 +0530

    Preprocessing dataset

--- unit4/preprocess.py
+++ unit4/preprocess.py
@@ -3,7 +3,7 @@ import string
 import pickle
 from nltk.corpus import stopwords
 
-stoplist = set(stopwords.words('english'))
+stoplist = list(set(stopwords.words('english')))+['rt']
 exclude = set(string.punctuation)
 
 def filter_out(line):
@@ -16,16 +16,43 @@ with open("FinalTweetList.csv", "r") as f:
     lines = f.readlines()
     lines = [line.strip() for line in lines if len(line.split(",")) == 2 and "@" in list(line)]
 
-result = []
+tag_to_name = {
+        '@TheOfficialSBI':'SBI',
+        '@HDFCBank_Cares':'HDFC',
+        '@ICICIBank_Care':'ICICI',
+        '@KotakBankLtd':'KOTAK',
+        '@ICICIBank':'ICICI',
+        '@RBI':'RBI',
+        '@AxisBankSupport':'AXIS',
+        '@HDFC_Bank':'HDFC',
+        '@AxisBank':'AXIS',
+        '@udaykotak':'KOTAK'
+    }
+    
+tag_keys = tag_to_name.keys()
+
+train = []
+test = dict()
 compiled = re.compile(r'@\w+')
 for line in lines:
     tags = re.findall(compiled, line)
     words = [w for w in line.split() if w not in tags]
     line = ' '.join(words)
-    text = filter_out(line.split(",")[0])
+    text = filter_out(line.split(",")[0]).strip()
     sentiment = line.split(",")[1]
+    tweet = [text, sentiment]
+    train.append(tweet)
     for tag in tags:
-        result.append([tag, text, sentiment])
+        if tag in tag_keys:
+            name = tag_to_name[tag]
+            if name in test.keys():
+                test[name].append(tweet)
+            else:
+                test[name] = [tweet]
         
-with open("dataset.bin", "wb") as f:
-    pickle.dump(result, f)
+with open("pickles/train.pkl", "wb") as f:
+    pickle.dump(train, f)
+    
+for k,v in test.items():
+    with open('pickles/test_'+k+'.pkl', 'wb') as f:
+        pickle.dump(v, f)
