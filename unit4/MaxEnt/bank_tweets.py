aba323460f151cf52329b00e72b5c7c353506eca
anirudhagar13 <anirudhagarwal13@gmail.com>
2016-11-15 21:30:13 +0530

    MaxEnt based on POS Tags

---
+++ unit4/MaxEnt/bank_tweets.py
@@ -0,0 +1,43 @@
+'''
+Created on 23-Jul-2016
+
+@author: Anantharaman
+'''
+import sys
+sys.path.append('/Work/Git/nlp-soln/unit4/MaxEnt')
+from feature_function_tweets import WikiFeatureFunctions
+from classifiers.maxent_base import LogLinear
+import os
+
+ds_path = os.path.join("..","data")
+
+def get_wiki_file_names():
+    fnames_1 = os.listdir(ds_path)
+    fnames = [os.path.join(ds_path, f) for f in fnames_1]
+    return fnames
+
+def prepare_dataset(supported_labels):
+    dataset = []
+    fnames_1 = os.listdir(ds_path)
+    fnames = [os.path.join(ds_path, f) for f in fnames_1]
+    for fn, fn1 in zip(fnames, fnames_1):
+        label = fn1.split("_")[1] # our convention is that the filename will be name_label
+        if label in supported_labels:
+            txt = open(fn).read()
+            txt = txt[:20]
+            dataset.append([txt, label])
+    return dataset
+
+if __name__ == '__main__':
+    ff = WikiFeatureFunctions()
+    supported_labels = ff.get_supported_labels()
+    print "Supported Labels: ", supported_labels
+    dataset = prepare_dataset(supported_labels)
+    clf = LogLinear(ff)
+    clf.train(dataset, max_iter=50)
+    while True:
+        txt = raw_input("Enter a text for classification: ")
+        if txt == "__Q__":
+            break
+        result = clf.classify(txt)
+        print result
