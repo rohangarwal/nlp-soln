13078061efa373290b9e8bdb4aeec952e12e6e90
Rohan <rohanagarwal6@gmail.com>
2016-11-15 18:01:05 +0530

    word2vec for all files

--- unit4/word2vec/word2vec_run.py
+++ unit4/word2vec/word2vec_run.py
@@ -1,4 +1,4 @@
-import gensim, pickle, random
+import gensim, pickle, random, os
 
 if __name__ == "__main__":
 
@@ -11,19 +11,23 @@ if __name__ == "__main__":
 	'''
 	
 	#test data
-	filename = '../pickles/test_SBI.pkl'
-	data = pickle.load(open(filename,'rb'))
-	
-	all_tweet_vec = list()
-	
-	for line in data:
-		tweet_vec = list()
-		for word in line[0].split():
-			if word in model.vocab:
-				tweet_vec.append(model[word])
-		all_tweet_vec.append([tweet_vec, line[1]])
-		
-	with open('tweet_vec_test_SBI.pkl', 'wb') as f:
-		pickle.dump(all_tweet_vec, f)
-		
-	#test data
+	dirname = '../pickles'
+	dirname2 = '../pickles/files'
+	add = 'tweet_vec_'
+	for fname in os.listdir(dirname):
+		if 'test' in fname:
+			#print fname
+			data = pickle.load(open(os.path.join(dirname, fname),'rb'))
+			all_tweet_vec = list()
+			
+			for line in data:
+				tweet_vec = list()
+				for word in line[0].split():
+					if word in model.vocab:
+						tweet_vec.append(model[word])
+				all_tweet_vec.append([tweet_vec, line[1]])
+				
+			filename = add + fname
+			with open(os.path.join(dirname2, filename), 'wb') as f:
+				pickle.dump(all_tweet_vec,f)
+
