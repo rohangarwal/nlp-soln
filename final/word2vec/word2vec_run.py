f90fe7a43a863f3d03dd7b7a681ccd5d08fc6a52
Rohan <rohanagarwal6@gmail.com>
2016-12-19 15:45:20 +0530

    word2vec files

---
+++ final/word2vec/word2vec_run.py
@@ -0,0 +1,20 @@
+import gensim, pickle, random, os
+
+if __name__ == "__main__":
+
+	model = gensim.models.Word2Vec.load('w2vmodel')
+	
+	filename = '../dataset/pickles/train_lines.pkl'
+	data = pickle.load(open(filename,'rb'))
+	all_vec = list()
+	for line in data:
+		vec = list()
+		for word in line[0].split():
+			if word in model.vocab:
+				vec.append(model[word])
+		if vec:
+			all_vec.append([vec,line[1]])
+
+	print len(all_vec)
+	with open('test_lines_vector.pkl','wb') as f:
+		pickle.dump(all_vec,f)
---
+++ final/word2vec/word2vec_train.py
@@ -0,0 +1,32 @@
+import gensim, logging, os,pickle
+logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
+
+class MySentences(object):
+    def __init__(self, filename):
+        self.datapkl = pickle.load(open(filename,'rb'))
+ 
+    def __iter__(self):
+    	for line in self.datapkl:
+    		yield line[0]
+
+if __name__ == "__main__":
+	
+	
+	#sentences = MySentences(filename) # a memory-friendly iterator
+	filename = '../dataset/pickles/train_phrases.pkl' #train word2vec
+	sentences = list()
+	datapkl = pickle.load(open(filename,'rb'))
+	for i in datapkl:
+		sentences.append([i[0].strip()])
+		#sentences.append(i[0].decode('utf-8'))
+
+	filename = '../dataset/pickles/test_phrases.pkl' #train word2vec
+	datapkl = pickle.load(open(filename,'rb'))
+	for i in datapkl:
+		sentences.append([i[0].strip()])
+	
+	#train word2vec on test sentences also - check performance.
+	#print sentences
+	model = gensim.models.Word2Vec(sentences,min_count=1,size=32)
+
+	model.save('w2vmodel')
